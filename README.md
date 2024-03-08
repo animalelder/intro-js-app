427d7066507404185984260952352e866f8d3fbd
animalelder <wllearned@icloud.com>
2024-03-08 12:20:59 -0800

    Clear script.js, Init Pokedex
    Replaced contents of script.js to begin storing Pokemon data in an array.
    
    Added 3 pokemon with 3 keys each: name, height, and types.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # intro-js-app
- Introduction to JS project
+The end result of this project is:
+
+To build a small web application with HTML, CSS, and JavaScript that loads
+data from an external API and enables the viewing of data points in detail.
+
+Users will be able to interact with the website and view details for a selected Pokemon.
--- js/script.js
+++ js/script.js
@@ -1,4 +1,30 @@
-alert('Hello world');
+/* Using an array, three random Pokemon will be added to an array called pokemonList
+    Each Pokemon will be an object with 3 key-value pairs of data items:
+        Name stored as a string
+        Height stored as an integer (rounded to whole number)
+        Types stored as an array of strings with at least one value */
 
-let favoriteFood = 'Fried rice'
-document.write(favoriteFood);
+// Create an array called pokemonList to store Pokemon data
+var pokemonList = [];
+
+
+// Add entry to pokemonList for Kakuna
+pokemonList[0] = {
+    name: 'Kakuna',
+    height: 2,
+    types: ['bug', 'poison'],
+};
+
+// Add entry to pokemonList for Charizard
+pokemonList[1] = {
+    name: 'Charizard',
+    height: 5,
+    types: ['fire', 'flying'],
+};
+
+// Add entry to pokemonList for Tentacool
+pokemonList[2] = {
+    name: 'Tentacool',
+    height: 3,
+    types: ['water', 'poison'],
+};
