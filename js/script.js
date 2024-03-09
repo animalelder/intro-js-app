d5597a4302bdce66d31fe5d3186e0f2d5a358af1
animalelder <wllearned@icloud.com>
2024-03-09 13:43:26 -0800

    Exercise 1.5 Part 2
    Updated script.js to use and IIFE with keys to add new entry and print all Pokemon
    
    Also adjusted height values to use hundredth of a decimal value.
    
    Created typeof check using if to make sure new data is an object. Attempted to add new item that is a string to see if it will be added.
    
    Added line to print getAll to console.log. String was not added to master list.
    
    Commented out old code, will delete for next assignment.

--- js/script.js
+++ js/script.js
@@ -1,34 +1,66 @@
 /* Using an array, three random Pokemon will be added to an array called pokemonList
     Each Pokemon will be an object with 3 key-value pairs of data items:
         Name stored as a string
-        Height stored as an integer (rounded to whole number)
+        Height stored as a float
         Types stored as an array of strings with at least one value */
 
 // Create an array called pokemonList to store Pokemon data
 var pokemonList = [];
 
+// IIFE for pokemonList storage with add and getAll functions
+let pokemonRepository = (function(){
 
-// Add entry to pokemonList for Kakuna
-pokemonList[0] = {
-    name: 'Kakuna',
-    height: 2,
-    types: ['bug', 'poison'],
-};
+let pokemonList = [
+    { name: 'Kakuna', height: 2.0, type: ['Bug', 'Poison']},
+    { name: 'Charizard', height: 5.6, type: ['Fire', 'Flying']},
+    { name: 'Tentacool', height: 3.92, type: ['Water', 'Poison']},
+];
 
-// Add entry to pokemonList for Charizard
-pokemonList[1] = {
-    name: 'Charizard',
-    height: 5,
-    types: ['fire', 'flying'],
-};
+function add(pokemon){
+    if (typeof pokemon === 'object'){
+    pokemonList.push(pokemon);
+    }
+}
 
-// Add entry to pokemonList for Tentacool
-pokemonList[2] = {
-    name: 'Tentacool',
-    height: 3,
-    types: ['water', 'poison'],
-};
+function getAll(){
+    return pokemonList;
+}
 
+return {
+    add: add,
+    getAll: getAll
+}
+})();
+
+// Add new Pokemon to pokemonList
+pokemonRepository.add({
+    name: 'Ivysaur', height: 3.25, type:['Grass', 'Poison']
+});
+
+// Test to see if 'typeof' within 'add()' will ignore any input that is not an object
+pokemonRepository.add('Pikachu');
+
+// Print all Pokemon data into console as a test.
+// Pikachu is not listed, typeof safeguard in IIFE is working
+console.log(pokemonRepository.getAll());
+
+// forEach function to print every Pokemon with 3 data fields
+// Pokemon with a height greater than 4 will add comment 'Wow, that's big!'
+
+pokemonRepository.getAll().forEach(function(pokemon){
+    if (pokemon.height > 4) {
+        document.write('<p>' + pokemon.name + ' (Height: ');
+        document.write(pokemon.height + ')');
+        document.write(' - Wow, that\'s big!' + '</p>')
+    }
+    else {
+        document.write('<p>' + pokemon.name + ' (Height: ');
+        document.write(pokemon.height + ')' + '</p>');
+    }
+})
+
+
+// **** OLD CODE BELOW, WILL BE DELETED SOON ****
 // PREVIOUS CODE
 
 // For loop to list Pokemon from array into index.html
@@ -48,17 +80,17 @@ pokemonList[2] = {
 // Refactor previous code to use forEach() rather than for loop
 
 // First create the function passed to forEach()
-function pokeLoopFunction(pokemon) {
-    if (pokemon.height > 3) {
-        document.write('<p>' + pokemon.name + ' (Height: ');
-        document.write(pokemon.height + ')');
-        document.write(' - Wow, that\'s big!' + '</p>')
-    }
-    else {
-        document.write('<p>' + pokemon.name + ' (Height: ');
-        document.write(pokemon.height + ')' + '</p>');
-    }
-}
+// function pokeLoopFunction(pokemon) {
+//     if (pokemon.height > 3) {
+//         document.write('<p>' + pokemon.name + ' (Height: ');
+//         document.write(pokemon.height + ')');
+//         document.write(' - Wow, that\'s big!' + '</p>')
+//     }
+//     else {
+//         document.write('<p>' + pokemon.name + ' (Height: ');
+//         document.write(pokemon.height + ')' + '</p>');
+//     }
+// }
 
 // Call pokeLoopFunction for each Pokemon
-pokemonList.forEach(pokeLoopFunction);
+// pokemonList.forEach(pokeLoopFunction);
