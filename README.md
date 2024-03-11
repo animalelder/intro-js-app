9a9fb5ac15efd8aa2bc18129b71cf0a899e3e74a
animalelder <wllearned@icloud.com>
2024-03-11 15:36:45 -0700

    Exercise 1.8: Modal
    script.js:
    
    Added Modal to display details of Pokemon
    
    Added eventListeners to close modal via X/Esc key/clicking outside the modal
    
    Adjusted the implementation of the loading message. I intend to replace it with a pre-made loader.
    
    Index.html:
    
    Adjusted #loadingMessage div to match JavaScript
    
    styles.css:
    
    Added rules for modal container, modal, and modal content.
    
    README.md:
    
    Updated to include some relevant information--not final version

--- README.md
+++ README.md
@@ -1,7 +1,27 @@
-# intro-js-app
-The end result of this project is:
+# Pokemon Pokedata
+
+## The end result of this project is:
 
 To build a small web application with HTML, CSS, and JavaScript that loads
 data from an external API and enables the viewing of data points in detail.
 
 Users will be able to interact with the website and view details for a selected Pokemon.
+
+## How to get the project running
+
+This project has not been uploaded to a server yet, so you can simply look at the code.
+
+## Project Dependencies
+
+- Created in Visual Studio Code
+- HTML5, CSS3, JavaScript ES2015
+- Tested with Safari and Brave on Mac OS, Safari on iOS
+- Extensions: Live Server, Prettier
+- pokemon API
+- Polyfill for promise function to support older browsers
+- Polyfill for fetch function to support older browsers
+- bootstrap to be added in future commit
+
+## APIs Used in This Project 
+
+- [pokeAPI](https://pokeapi.co/api/v2/pokemon/1/)
--- js/script.js
+++ js/script.js
@@ -7,12 +7,99 @@
 // Create an array called pokemonList to store Pokemon data
 var pokemonList = [];
 
-// IIFE for pokemonList storage with add and getAll functions
-let pokemonRepository = (function(){
 
+// IIFE for pokemonList storage with add and getAll functions
+// All the functions inside the IIFE can be called from outside the IIFE
+// but they are sort of protected in there, separated, and have only a few global var
+let pokemonRepository = (function(){
+  
+    // Define modalContainer by #modal-container
+    let modalContainer = document.querySelector('#modal-container');
+    // Create pokemonList object and variable for apiURL
     let pokemonList = [];
     let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
+        // Load the details per Pokemon
+        function loadDetails(item) {
+          showLoadingMessage();
+          let url = item.detailsUrl;
+          return fetch(url).then(function (response) {
+            return response.json();
+          }).then(function (details) {
+              hideLoadingMessage();
+              item.imageUrl = details.sprites.front_shiny;
+              item.height = details.height;
+          }).catch(function (e) {
+              hideLoadingMessage();
+              console.error(e);
+          });
+        }
+      
+  
+    
+        // Show details per pokemon
+      function showDetails(pokemon){
+          loadDetails(pokemon).then( function (){
+          openModal(pokemon);
+      })
+      }
+
+    function closeModal(pokemon){
+      modalContainer.classList.remove('is-visible');
+    }
+
+    function openModal(pokemon){
+      // Clear modal
+      modalContainer.innerHTML = '';
+
+      // Create the modal elements
+      let modal = document.createElement('div');
+      modal.classList.add('modal');
+
+      let modalContent = document.createElement('div');
+      modalContent.classList.add('modal-content');
+
+      let closeButton = document.createElement('span');
+      closeButton.classList.add('modal-close');
+      closeButton.style.cssFloat='right';
+      closeButton.innerHTML = '&times;';
+      closeButton.addEventListener('click', closeModal);
+
+      let nameElement = document.createElement('h2');
+      nameElement.innerText = pokemon.name;
+
+      let heightElement = document.createElement('p');
+      heightElement.innerText = 'Height: ' + pokemon.height;
+
+      let imageElement = document.createElement('img');
+      imageElement.src = pokemon.imageUrl;
+      imageElement.alt = pokemon.name;
+
+      // Append the elements to the content div
+      modalContent.appendChild(closeButton);
+      modalContent.appendChild(nameElement);
+      modalContent.appendChild(heightElement);
+      modalContent.appendChild(imageElement);
+
+      // Append the content to the modal
+      modal.appendChild(modalContent);
+
+      modalContainer.appendChild(modal);
+
+      modalContainer.classList.add('is-visible');
+
+      modalContainer.addEventListener('click', (e) => {
+        // This is trigger when clicking inside the modal
+        // We only want to close if the user clicks directly on
+        // the overlay
+        let target = e.target;
+        if (target === modalContainer) {
+          closeModal();
+        }
+      })
+    }
+  
+    // This will load the list of 150 pokemon via the api
     function loadList() {
         showLoadingMessage();
         return fetch(apiUrl).then(function (response) {
@@ -33,6 +120,7 @@ let pokemonRepository = (function(){
         })
       }
 
+    // This is to add one pokemon, it will be called in loadList() above  
     function add(pokemon){
         if (
             typeof pokemon === "object" &&
@@ -44,10 +132,12 @@ let pokemonRepository = (function(){
           }     
     }
 
+    // This function will get all the Pokemon, is called below outside IIFE
     function getAll(){
         return pokemonList;
     }
 
+    // Similar to getAll(), this will add elements for each Pokemon from our list to our page
     function addListItem(pokemon){
         let container = document.querySelector('.pokemon-list');
         let listItem = document.createElement('li');
@@ -59,32 +149,14 @@ let pokemonRepository = (function(){
         addButtonListener(button, pokemon);
     }
 
+    // This function will listen for clicks on the button created in addListItem() above
     function addButtonListener(button, pokemon){
         button.addEventListener('click', function() {showDetails(pokemon)});
     }
     
-    function loadDetails(item) {
-        showLoadingMessage();
-        let url = item.detailsUrl;
-        return fetch(url).then(function (response) {
-          return response.json();
-        }).then(function (details) {
-            hideLoadingMessage();
-            item.imageUrl = details.sprites.front_default;
-            item.height = details.height;
-        }).catch(function (e) {
-            hideLoadingMessage();
-            console.error(e);
-        });
-      }
-    
-    function showDetails(pokemon){
-        loadDetails(pokemon).then( function (){
-        console.log(pokemon);
-    })
-    }
 
 
+    // Every IIFE must return the functions inside at the end
     return {
         loadList: loadList,
         add: add,
@@ -92,7 +164,9 @@ let pokemonRepository = (function(){
         addListItem: addListItem,
         loadDetails: loadDetails,
         showDetails: showDetails,
-        addButtonListener: addButtonListener
+        addButtonListener: addButtonListener,
+        openModal: openModal,
+        closeModal: closeModal
     }
 })();
 
@@ -103,23 +177,28 @@ pokemonRepository.loadList().then(function() {
     });
   });
 
+// This was the bonus task to add a loading message
+// It came out looking amateur, but it works
 function showLoadingMessage(){
-    document.getElementById('loadingMessage').style.display = 'flex';
+    document.getElementById('loadingMessage').classList.add('is-visible');
     console.log('Loading.');
 }
 
+// Hide loading message when Loading is finished
+// Right now, setTimeout delays the loading message from disappearing so quickly
 function hideLoadingMessage(){
   setTimeout(function() {
-    document.getElementById('loadingMessage').style.display = 'none';
+    document.getElementById('loadingMessage').classList.remove('is-visible');
     console.log('Finished Loading.');
 }, 1000)};
 
-// OLD CODE  
-// forEach function to call addListItem to create elements for each Pokemon
-//   pokemonRepository.getAll().forEach(function (pokemon) {
-//       pokemonRepository.addListItem(pokemon);
-//     });
-    
-    
+// Listen in the window for the Escape key to close the modal
+window.addEventListener('keydown', (e) => {
+  let modalContainer = document.querySelector('#modal-container');
+  if (e.key === 'Escape' && modalContainer.classList.contains('is-visible'))
+  {
+    pokemonRepository.closeModal();
+  }
+});
     
     
